44c6b0e7191628f4f05960b760a1f2f0e16af2fa
Henrique Freire <raimundohfneto@gmail.com>
2023-09-14 15:34:08 -0300

    Criação do projeto

---
+++ GeradorSenhas/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textcolor}>Vamos da inicio aos trabalhos. hehehe</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textcolor: {
+    color: '#ff0000',
+    marginTop: 10,
+  },
+});
