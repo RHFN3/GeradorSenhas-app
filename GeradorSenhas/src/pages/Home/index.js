50cc2f1974bc0cf084b3bb9d601f85a16626dbda
Henrique Freire <raimundohfneto@gmail.com>
2023-09-19 14:06:52 -0300

    Adc Routes.js / Adc tabBar

---
+++ GeradorSenhas/src/pages/Home/index.js
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import Slider from '@react-native-community/slider';
+import {ModalPassword} from '../../components/modal';
+
+let charset = ".+-,/*qwertyuioipçlkjhgfdsdazxcvbnmQWERTYUIOPÇLKJHGFDSAZXCVBNM0123456789"
+
+export function Home() {
+  const [size, setSize] = useState(10);
+  const [passwordValue, setPasswordValue] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function generatePassword(){
+    
+    let password = "";
+    for (let i = 0, n = charset.length; i < size; i++){
+      password += charset.charAt(Math.floor(Math.random() * n))
+    }
+    setPasswordValue(password)
+    setModalVisible(true)
+
+  }
+
+
+  return (
+    <View style={styles.container}>
+
+    <Image source={require("../../assets/logo.png")} style = {styles.logo}/>
+
+    <Text style={styles.title}>{size} Caracteres</Text>
+    
+    <View style={styles.area}>
+      <Slider
+      style={{ height: 50}}
+      minimumValue={8}
+      maximumValue={20}
+      maximumTrackTintColor='#51C94F'
+      minimumTrackTintColor='#ECA2A3'
+      thumbTintColor='#00B1B9'
+      value={size}
+      onValueChange={ (value) => setSize(value.toFixed(0))}
+      />
+    </View>
+
+    <TouchableOpacity style={styles.button} onPress={generatePassword}>
+      <Text style={styles.buttonText}>Gerar senha</Text>
+    </TouchableOpacity>
+
+    <Modal visible={modalVisible} animationType='fade' transparent={true}>
+      <ModalPassword password={passwordValue} handleClose={ () => setModalVisible(false)}/>
+    </Modal>
+    
+    <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#DEDEDE',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    marginBottom: 15,
+    marginTop: 0,
+    width: 200,
+    height: 200,
+
+  },
+  title: {
+    fontSize: 20,
+    fontWeight:"bold",
+
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 6,
+  },
+  button: {
+    backgroundColor: "#00B1B9",
+    width: "80%",
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 18,
+  },
+});
